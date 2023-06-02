3ac1466995c8177f68fe606a0458ba3fa4bc4b9c
Indirachetty <118746300+Indira-chetty@users.noreply.github.com>
2023-06-02 22:44:57 +0530

    Create ismultiple.java

---
+++ ismultiple.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class UserMainCode
+
+{
+
+public int isMultiple(int inputil,int input2){
+
+if(input2==0|| input1==0){
+return 3;
+}
+if (input1%input2==0){
+return 2;
+}
+else{
+return 1;
+
+}
+}
+}
