f2c094efacf18a6bce0d2a8ce372981a9258e00f
Indirachetty <118746300+Indira-chetty@users.noreply.github.com>
2023-06-02 22:51:24 +0530

    Create countEven.java

---
+++ countEven.java
@@ -0,0 +1,25 @@
+import java util.*;
+class Usermaincode{
+public int countEvens(int inputl,int input2,int input3,int input4,int input5){
+int count=0;
+if(inputl1%2==0){
+count++;
+}
+if(input2%2==0){
+count++;
+}
+if(input3%2==0){
+count++;
+}
+if(inputa%2==0){
+count++;
+}
+if(inputs%2==0){
+count++;
+
+}
+
+return count;
+
+}
+}
