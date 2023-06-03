eec240dc0b67df5c5186ae92ab507d2ad579c0fd
Indirachetty <118746300+Indira-chetty@users.noreply.github.com>
2023-06-03 14:42:29 +0530

    Create isprime.java

---
+++ isprime.java
@@ -0,0 +1,19 @@
+class Usermaincode
+{
+public int isprime(int input1)(
+int count=0;
+if(input1==0 || input1=1){
+return 1;
+}
+if (input1==2){
+ return 2
+)
+if(input1%2!=0 & input1%3!=0){
+count=count+1;
+}
+if(count==1)(
+return 2;
+}
+else
+return 1;
+}
