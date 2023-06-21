41c0d5d479666adea239643c0c7b788733ffe143
Indirachetty <118746300+Indira-chetty@users.noreply.github.com>
2023-06-21 15:36:54 +0530

    Create alldigitsCount.java

---
+++ alldigitsCount.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+// Read only region start
+class UserMainCode
+
+{
+
+public int alldigitsCount(int inputl){
+// Read only region end
+// write code here...
+//throw new UnsupportedoperationException(*allbigitsCount(int input1)");
+String a=Integer.toString(input1);
+
+return a.length();
+}
+}
