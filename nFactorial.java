05c031489d971285f1711fb753a70203948d7fcd
Indirachetty <118746300+Indira-chetty@users.noreply.github.com>
2023-06-21 14:35:51 +0530

    Create nFactorial.java

---
+++ nFactorial.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+// Read only region start
+class UserMainCode
+
+{
+
+public int nFactorial(int input1){
+// Read only region end
+// Write code here...
+//throw new UnsupportedOperationException(“"nFactorial(int input1)");
+int fact=1;
+for(int i=1;i<=inputl;i++){
+fact=fact*i; 
+}
+
+return fact;
+}}
