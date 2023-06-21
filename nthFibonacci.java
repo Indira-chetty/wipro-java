69c67c437d9dbbb4a1ad2277c5f6f0fec82f95f6
Indirachetty <118746300+Indira-chetty@users.noreply.github.com>
2023-06-21 15:11:15 +0530

    Create nthFibonacci.java

---
+++ nthFibonacci.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+// Read only region start
+class UserMainCode
+
+{
+
+public long nthFibonacci(int input1){
+// Read only region end
+// Write code here...
+//throw new UnsupportedoperationException(”nthFibonacci (int input1)");
+int a=0,b=1,c=0;
+if(inputl==1||input1==0){
+
+| return 0;
+}
+else{
+if (input1>1){
+
+for (int i=2;icinputl;is+){
+
+c=atb;
+ab;
+b=c;
+
+}
+
+}
+return c;
+}
+}
+
